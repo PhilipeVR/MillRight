660a23471f988ca67d596b3e74274751f68cadb2
Philipe Rosier <prosi069@uottawa.ca>
2020-07-12 19:20:43 -0400

    FrenchDialogCommit

--- Z5_Mill/Assets/Scripts/DialogueManager.cs
+++ Z5_Mill/Assets/Scripts/DialogueManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,31 +11,51 @@ public class DialogueManager : MonoBehaviour
     public Text dialogueText;
     public Text dialogIndex;
     public GameObject Image;
+    public Boolean language = true;
+
     private Sprite[] images;
     private int count;
     private int index;
-
     private Queue<string> sentences;
+    private Queue<string> sentencesFR;
+    private Queue<string> currentLangSentence;
+    private string lastSentence;
+    private string titleLang;
 
     void Awake () {
         sentences = new Queue<string>();
-        
-
+        sentencesFR = new Queue<string>();
         index = 1;
     }
 
     public void StartDialogue (Dialogue dialogue)
     {
         nameText.text = dialogue.name;
+        titleLang = dialogue.frenchName;
         sentences.Clear();
 
         foreach (string sentence in dialogue.sentences)
         {
             sentences.Enqueue(sentence);
         }
+
+        foreach(string sentece in dialogue.sentencesFR)
+        {
+            sentencesFR.Enqueue(sentece);
+        }
+
         count = sentences.Count;
 
         images = dialogue.images;
+
+        if (language)
+        {
+            currentLangSentence = sentences;
+        }
+        else
+        {
+            currentLangSentence = sentencesFR;
+        }
 
         DisplayNextSentence();
     }
@@ -46,7 +67,15 @@ public class DialogueManager : MonoBehaviour
             EndDialogue();
             return;
         }
-        string sentence = sentences.Dequeue();
+        string sentence = currentLangSentence.Dequeue();
+        if (language)
+        {
+            lastSentence = sentencesFR.Dequeue();
+        } else
+        {
+            lastSentence = sentences.Dequeue();
+        }
+
         dialogueText.text = sentence;
         dialogIndex.text = index + "/" + count;
         Image.GetComponent<Image>().sprite = images[index - 1];
@@ -61,4 +90,24 @@ public class DialogueManager : MonoBehaviour
         Debug.Log("End of conversation.");
         DeactivateAtEndDialogue.SetActive(false);
     }
+
+     public void switchLang() {
+        if (language)
+        {
+            currentLangSentence = sentencesFR;
+        } else
+        {
+            currentLangSentence = sentences;
+        }
+
+        string tmp = nameText.text;
+        nameText.text = titleLang;
+        titleLang = tmp;
+
+        string tmp2 = dialogueText.text;
+        dialogueText.text = lastSentence;
+        lastSentence = tmp2;
+
+        language = !language;
+     }
 }
---
+++ Z5_Mill/Assets/Scripts/ToggleText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private string initialText;
+    public string toggleOptionText;
+    public Text currentText;
+    void Start()
+    {
+        initialText = currentText.text ;
+        Debug.LogWarning("initial");
+
+    }
+
+    // Update is called once per frame
+    public void toggle()
+    {
+        Debug.LogWarning("Toggle");
+
+        if (currentText.text.Equals(initialText))
+        {
+            currentText.text = toggleOptionText;
+        } else
+        {
+            currentText.text = initialText;
+
+        }
+        Debug.LogWarning(currentText.text);
+
+    }
+}
