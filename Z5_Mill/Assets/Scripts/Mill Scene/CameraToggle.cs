081c0f7adc56e4369b6b18cfc8ef6e8b17f47344
Philipe Rosier <prosi069@uottawa.ca>
2020-07-13 04:54:00 -0400

    BaseComponentDetailMillSceneCommit

---
+++ Z5_Mill/Assets/Scripts/Mill Scene/CameraToggle.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.ConstrainedExecution;
+using UnityEngine;
+
+public class CameraToggle : MonoBehaviour
+{
+    Camera[] cameras;
+    [SerializeField] string genericCameraTag;
+    private static string MAIN_CAM_TAG = "MainCamera"; 
+    private int index;
+    void Awake()
+    {
+        index = 0;
+        cameras = GetComponentsInChildren<Camera>();
+        UnityEngine.Debug.LogWarning("Cameras: " + cameras.Length.ToString());
+        deactivateAll();
+        SwtichView();
+        
+    }
+
+    // Update is called once per frame
+    public void SwtichView()
+    {
+        UnityEngine.Debug.LogWarning("Camera: " + index.ToString());
+
+        if (index == cameras.Length)
+        {
+            cameras[cameras.Length - 1].tag = genericCameraTag;
+            cameras[cameras.Length - 1].enabled = false;
+            index = 0;
+        } else if(index != 0)
+        {
+            cameras[index - 1].tag = genericCameraTag;
+            cameras[index - 1].enabled = false;
+        }
+        cameras[index].enabled = true;
+        cameras[index].tag = MAIN_CAM_TAG;
+        index++;
+    }
+
+    void deactivateAll()
+    {
+        for(int i = 0; i<cameras.Length; i++)
+        {
+            cameras[i].enabled = false;
+        }
+    }
+}
---
+++ Z5_Mill/Assets/Scripts/Mill Scene/OnHover.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnHover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Color hoverColor;
+    private Color basicColor;
+    private Renderer renderer;
+    [SerializeField] private int detailIndex;
+    [SerializeField] GameObject ObjectManager;
+    private ComponentManager manager;
+
+
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        basicColor = renderer.material.color;
+        manager = ObjectManager.GetComponent<ComponentManager>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (transform.gameObject.Equals(hit.collider.gameObject))
+                {
+                    manager.SetDetails(detailIndex);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void OnMouseEnter()
+    {
+        renderer.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        renderer.material.color = basicColor;
+    }
+}
