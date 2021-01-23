525c1c907115e03d8496485717bc04748967e1dd
Philipe Rosier <prosi069@uottawa.ca>
2021-01-23 13:00:34 -0500

    MillRightBuildCommit

---
+++ Mill Right/Assets/MILL/Scripts/Scene Management Scripts/InputFieldShorcuts.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class InputFieldShorcuts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject InputPanel;
+    [SerializeField] private InputField firstInputField;
+    [SerializeField] private InputField secondInputField;
+    [SerializeField] private Button sumbitBTN;
+    private EventSystem system;
+    private void Start()
+    {
+        system = EventSystem.current;
+    }
+    private void OnGUI()
+    {
+        if (InputPanel.activeSelf)
+        {
+            Event e = Event.current;
+            if (e.type == EventType.KeyDown && e.keyCode == KeyCode.Tab)
+            {
+                if (system.currentSelectedGameObject.Equals(firstInputField.gameObject))
+                {
+                    system.SetSelectedGameObject(secondInputField.gameObject, new BaseEventData(system));
+                }
+            }
+            else if (e.type == EventType.KeyDown && e.keyCode == KeyCode.Return)
+            {
+                sumbitBTN.onClick.Invoke();
+            }
+        }
+    }
+
+
+
+}
