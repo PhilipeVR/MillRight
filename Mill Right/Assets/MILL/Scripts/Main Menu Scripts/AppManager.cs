4d3329a2ff963bcc6a1ab3e9098fdd5e95798a81
D-Neptune <dneptune2013@gmail.com>
2021-02-22 12:29:43 -0500

    Added AppManager to MainMenu scene to control target framerate

---
+++ Mill Right/Assets/MILL/Scripts/Main Menu Scripts/AppManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour
+{
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 0; // Must be zero for targetFrameRate to work
+        Application.targetFrameRate = 30;
+        DontDestroyOnLoad(this.gameObject); // Preserves AppManager across scenes
+    }
+}
+
